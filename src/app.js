c47541ddeebc26992855be9b59b37f44b712b8c4
patelmadhav812-max <patelmadhav812@gmail.com>
2026-09-18 14:31:57 +0530

    add default route

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Routes
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Bank Backend API is running successfully 🚀",
+  });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/account", accountRoutes);
 app.use("/api/transactions", transactionRoutes);
